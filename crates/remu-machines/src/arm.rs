db44b245dcec53c9ec9118813c8840ae705adbf2
Joshua Scarsbrook <j.scarsbrook@uq.edu.au>
2026-08-03 19:18:11 +1000

    Tighten RP2040 UART1 qualification

--- crates/remu-machines/src/arm.rs
+++ crates/remu-machines/src/arm.rs
@@ -1471,22 +1471,4 @@ impl ArmMachine {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn both_raspberry_pi_arm_profiles_construct() {
-        let mut rp2040 = ArmMachine::new(TargetId::Rp2040).unwrap();
-        ArmMachine::new(TargetId::Rp2350).unwrap();
-        rp2040
-            .bus
-            .write(
-                0x4003_8000,
-                AccessWidth::Word,
-                u64::from(b'Z'),
-                SimTime::ZERO,
-            )
-            .unwrap();
-        assert_eq!(rp2040.chip_uart1.as_ref().unwrap().bytes(), b"Z");
-    }
-}
+mod tests;
