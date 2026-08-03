3e817835d7b49dcf91039006bc90ae90f409f921
Joshua Scarsbrook <j.scarsbrook@uq.edu.au>
2026-08-03 21:11:01 +1000

    Audit RP2350 TICKS register semantics

--- crates/remu-devices/src/tests.rs
+++ crates/remu-devices/src/tests.rs
@@ -688,10 +688,29 @@ fn rp2350_accessctrl_tracks_masks_locks_and_configuration_reset() {
 #[test]
 fn rp2350_ticks_expose_independent_running_and_countdown_state() {
     let mut ticks = Rp2350Ticks::new("ticks");
+    assert_eq!(
+        Rp2350TicksRegister::try_from(0x44).unwrap(),
+        Rp2350TicksRegister::RiscvCount
+    );
+    assert!(Rp2350TicksRegister::try_from(0x48).is_err());
     assert_eq!(ticks.read(0, AccessWidth::Word, SimTime::ZERO).unwrap(), 0);
     ticks
         .write(4, AccessWidth::Word, 4, SimTime::from_ticks(10))
         .unwrap();
+    ticks
+        .write(
+            0,
+            AccessWidth::Word,
+            u64::from(u32::MAX),
+            SimTime::from_ticks(10),
+        )
+        .unwrap();
+    assert_eq!(
+        ticks
+            .read(0, AccessWidth::Word, SimTime::from_ticks(10))
+            .unwrap(),
+        3
+    );
     ticks
         .write(0, AccessWidth::Word, 1, SimTime::from_ticks(10))
         .unwrap();
